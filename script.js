cfc94203e6deae9f2d131d3bcd957b9a65761a6a
LuizMenezess <luizmenezes334@gmail.com>
2021-05-28 18:29:21 -0300

    modificando código

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let aboutMe = {
     sister: "Angela",
     brother: "Davi"
 }
+console.log(aboutMe);
 
 //02-Adicione uma nova propriedade sem alterar seu objeto inicial:
 aboutMe.time = "Flamengo"
@@ -24,50 +25,45 @@ delete aboutMe.time
 //04-Mostre no console todas as propriedades desse objeto:
 console.log(aboutMe);
 
-
 //05- Crie um array  chamado "cadastro" contendo ao menos 5 objetos. Na propriedade amigos, adicione ao menos 4 itens:
 let cadastro = [
 {
     nome: "Daniel",
     idade: 21,
-    telefone: 1234-1234,
-    amigos: `Marcia, Lucas, Alisson e Joao`
+    telefone: "1234-1234",
+    amigos: ["Marcia", "Lucas", "Alisson", "Joao", "Ronan"]
 },
 
 {
     nome: "Sabrina",
     idade: 17,
-    telefone: 1234-1234,
-    amigos: `Maria, Luan, Alice e Luiza`
+    telefone: "5431-1543",
+    amigos: ["Maria", "Luan", "Alice", "Luiza", "Tati"]
 },
 
 {
     nome: "Esther",
     idade: 11,
-    telefone: 1234-1234,
-    amigos: `Pedro, Marlon, Lucas e Bianca`
+    telefone: "4876-1020",
+    amigos: ["Pedro", "Marlon", "Lucas", "Bianca", "Jason"]
 },
 
 {
     nome: "Romildo",
     idade: 51,
-    telefone: 1234-1234,
-    amigos: `Bia, Bruno, Carla e Karol`
+    telefone: "3401-1416",
+    amigos: ["Bia", "Bruno", "Carla", "Karol", "Renan"]
 },
 
 {
     nome: "Sheila",
     idade: 21,
-    telefone: 1234-1234,
-    amigos: `Bruna, luiz, Jose e Caio`
+    telefone: "1234-1764",
+    amigos: ["Bruna", "luiz", "Jose", "Caio", "Thales"]
 }
-
 ]
-console.log(cadastro)
+
+console.log(cadastro);
 
 //06-Mostre no console o nome de um amigo de cada lista:
-console.log(cadastro[0].amigos[3]) 
-console.log(cadastro[1].amigos[2])
-console.log(cadastro[1].amigos[4])
-console.log(cadastro[0].amigos[1])
-console.log(cadastro[4].amigos[3])
+console.log(cadastro[0].amigos[1], cadastro[1].amigos[2], cadastro[2].amigos[3],cadastro[3].amigos[4], cadastro[4].amigos[0]);
