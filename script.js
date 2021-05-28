2ec0e2add5aec28a357770c16c326b4ef9d11b69
LuizMenezess <luizmenezes334@gmail.com>
2021-05-28 17:58:43 -0300

    modificando arquivo script.js

--- script.js
+++ script.js
@@ -14,21 +14,15 @@ let aboutMe = {
     sister: "Angela",
     brother: "Davi"
 }
-console.log(aboutMe);
-
 
 //02-Adicione uma nova propriedade sem alterar seu objeto inicial:
 aboutMe.time = "Flamengo"
-console.log(aboutMe);
-
 
 //03-Remova uma propriedade desse objeto:
 delete aboutMe.time
-console.log(aboutMe);
-
 
 //04-Mostre no console todas as propriedades desse objeto:
-console.log(aboutMe.dog, aboutMe.sister, aboutMe.brother);
+console.log(aboutMe);
 
 
 //05- Crie um array  chamado "cadastro" contendo ao menos 5 objetos. Na propriedade amigos, adicione ao menos 4 itens:
@@ -36,44 +30,44 @@ let cadastro = [
 {
     nome: "Daniel",
     idade: 21,
-    telefone: 0000-0000,
+    telefone: 1234-1234,
     amigos: `Marcia, Lucas, Alisson e Joao`
 },
 
 {
     nome: "Sabrina",
     idade: 17,
-    telefone: 0000-0000,
+    telefone: 1234-1234,
     amigos: `Maria, Luan, Alice e Luiza`
 },
 
 {
     nome: "Esther",
     idade: 11,
-    telefone: 0000-0000,
+    telefone: 1234-1234,
     amigos: `Pedro, Marlon, Lucas e Bianca`
 },
 
 {
     nome: "Romildo",
     idade: 51,
-    telefone: 0000-0000,
+    telefone: 1234-1234,
     amigos: `Bia, Bruno, Carla e Karol`
 },
 
 {
     nome: "Sheila",
     idade: 21,
-    telefone: 0000-0000,
+    telefone: 1234-1234,
     amigos: `Bruna, luiz, Jose e Caio`
 }
 
 ]
 console.log(cadastro)
 
-
 //06-Mostre no console o nome de um amigo de cada lista:
-console.log(cadastro[0].amigos, cadastro[1].amigos, cadastro[2].amigos, cadastro[3].amigos, cadastro[4].amigos);
-
-
-//atividades 05 e 06 não está finalizado!
+console.log(cadastro[0].amigos[3]) 
+console.log(cadastro[1].amigos[2])
+console.log(cadastro[1].amigos[4])
+console.log(cadastro[0].amigos[1])
+console.log(cadastro[4].amigos[3])
